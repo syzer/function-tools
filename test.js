ee74296fef530ae333cad5122e3677cffa00314c
syzer3@gmail.com <syzer3@gmail.com>
2017-01-17 09:25:18 +0100

    test using stream api

--- test.js
+++ test.js
@@ -1,11 +1,19 @@
 import test from 'ava'
+import ost from 'object-stream-tools'
 import { readFile, readFileStream, writeFile } from './index'
 
-test(t => {
-    const test = readFile(__dirname + '/README.txt')
+test('map and fork works ok', t => {
+    const test = readFile(__dirname + '/README.md')
         .map(e => e.split(/\n|\t/gi))
-        .chain(contents => writeFile(__dirname + '/.tmp.txt', contents))
+        .chain(contents => writeFile(__dirname + '/.tmp.md', contents))
 
     test.fork(() => t.fail('Error running file'), () => t.pass())
 })
 
+test('can pipe', t => {
+    const test = readFileStream(__dirname + '/README.md')
+        .map(ost.map(e => e.split(/\n|\t/gi))
+        .pipe(process.stdout))
+
+    test.fork(() => t.fail('Error running file'), () => t.pass())
+})
